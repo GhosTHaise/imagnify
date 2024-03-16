b3f98dd503e6cf336a9f8dcfb778ab72c85d9ba1
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-03-16 21:04:06 +0300

    feat : mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Mobilenav from '@/components/shared/Mobilenav'
 import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
 
@@ -5,7 +6,7 @@ const Layout = ({children} : {children : React.ReactNode}) => {
   return (
     <main className="root">
       <Sidebar /> 
-      {/* <MobileNav /> */}
+      <Mobilenav />
         <div className='root-container'>
             <div className='wrapper'>
                 {children}
