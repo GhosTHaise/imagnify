1928c0780db2103e3bfdcf513365b80f8a7547ab
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-03-17 21:12:44 +0300

    feat : add sheet to nav mobile

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -6,6 +6,7 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import { SignedIn, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
   
@@ -23,6 +24,26 @@ const Mobilenav = () => {
                     height={28}
                 />
         </Link>
+
+        <nav className="flex gap-2">
+            <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+                <Sheet>
+                  <SheetTrigger>
+                    <Image
+                      src="/assets/icons/menu.svg"
+                      alt="menu"
+                      width={32}
+                      height={32}
+                      className="cursor-pointer"
+                    />
+                  </SheetTrigger>
+                  <SheetContent className="sheet-content sm:w-64">
+                      
+                  </SheetContent>
+                </Sheet>
+            </SignedIn>
+        </nav>
     </header>
   )
 }
