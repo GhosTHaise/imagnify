fb3f13b3bd5f582fc589f4c5a151decbcaca4d8c
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-03-19 19:47:28 +0300

    faet : conncet database

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,31 @@
+import mongoose , { Mongoose } from "mongoose"
+
+const MONGOBD_URL = process.env.MONGO_URL;
+
+interface MongooseConnection {
+    conn : Mongoose | null;
+    promise : Promise<Mongoose> | null;
+}
+
+let cached : MongooseConnection = (global as any).mongoose
+
+if(!cached){
+    cached = (global as any).mongoose = {
+        conn : null,
+        promise : null
+    }
+}
+
+export const connectDatabase = async() => {
+    if(cached.conn) return cached.conn;
+    
+    if(!MONGOBD_URL) throw new Error("Midding MONGODB_URL");
+
+    cached.promise = cached.promise
+    || 
+    mongoose.connect(MONGOBD_URL,{dbName : "imaginify",bufferCommands : false});
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+}
