1134198b18ba10f501b9013f5529acb8ba07dbee
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-04-13 20:03:53 +0300

    feat : show transformed image

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
+import { CldImage } from 'next-cloudinary'
+import { dataUrl, debounce, getImageSize } from '@/lib/utils'
+import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 
 const TransformedImage = ({
   image,
@@ -9,40 +12,60 @@ const TransformedImage = ({
   setIsTransforming,
   transformationConfig,
   hasDownload = false
-} : TransformedImageProps) => {
+}: TransformedImageProps) => {
 
-  const downloadHandler = (e : any) => {
+  const downloadHandler = (e: any) => {
 
   }
 
   return (
     <div className='flex flex-col gap-4'>
       <div className='flex-between'>
-          <h3 className='h3-bold text-dark-600'>
-              Transformed
-          </h3>
+        <h3 className='h3-bold text-dark-600'>
+          Transformed
+        </h3>
 
-          {
-            hasDownload && (
-              <button
-                type='button'
-                className='download-btn' 
-                onClick={(e) => downloadHandler(e)}>
-                  <Image
-                      src={"/assets/icons/download.svg"}
-                      alt='Download'
-                      width={24}
-                      height={24}
-                      className='pb-[6px]'
-                  />
-              </button>
-            )
-          }
+        {
+          hasDownload && (
+            <button
+              type='button'
+              className='download-btn'
+              onClick={(e) => downloadHandler(e)}>
+              <Image
+                src={"/assets/icons/download.svg"}
+                alt='Download'
+                width={24}
+                height={24}
+                className='pb-[6px]'
+              />
+            </button>
+          )
+        }
       </div>
       {
         image?.publicId && transformationConfig ? (
           <div className='relative'>
-              
+            <CldImage
+              width={getImageSize(type, image, "width")}
+              height={getImageSize(type, image, "height")}
+              src={image?.publicId}
+              alt={image?.title}
+              sizes={"(max-width : 767px 100vw, 50vw"}
+              placeholder={dataUrl as PlaceholderValue}
+              className="transformed-image"
+              onLoad={() => {
+                setIsTransforming && setIsTransforming(false)
+              }}
+              onError={() => {
+                debounce(() => {
+                  setIsTransforming && setIsTransforming(false)
+                },8000)
+              }}
+              {
+                ...transformationConfig
+              }
+            />
+            
           </div>
         ) : (
           <div className='transformed-placeholder'>
