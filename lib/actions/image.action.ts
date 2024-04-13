1f137dca690b145189f44217f7d14b30e914cb6b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-04-13 21:30:03 +0300

    feat : add image actions

--- lib/actions/image.action.ts
+++ lib/actions/image.action.ts
@@ -3,8 +3,33 @@
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
+import User from "../database/models/user.model";
+import Image from "../database/models/image.model";
 
 export async function addImage({ image, userId, path }: AddImageParams) {
+  try {
+    await connectToDatabase();
+
+    const author = await User.findById(userId);
+    if (!author) {
+      throw new Error("User not found");
+    }
+
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    });
+    //don't return cache
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(newImage));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// UPDATE IMAGE
+export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
 
@@ -12,6 +37,34 @@ export async function addImage({ image, userId, path }: AddImageParams) {
     revalidatePath(path);
 
     return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+//DELETE IMAGE
+export async function deleteImage(imageId: string) {
+  try {
+    await connectToDatabase();
+
+    //don't return cache
+    //revalidatePath(path);
+
+    //return JSON.parse(JSON.stringify(image))
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+//GET IMAGE
+export async function getImageById(imageId: string) {
+  try {
+    await connectToDatabase();
+
+    //don't return cache
+    //revalidatePath(path);
+
+    //return JSON.parse(JSON.stringify(image))
   } catch (error) {
     handleError(error);
   }
