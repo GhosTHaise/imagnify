03ae9d81bcad9daaee435425fb88e186bd719d5b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-04-21 19:57:01 +0300

    feat : images action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Home = ({ searchParams} : SearchParamProps) => {
+const Home = ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
-  const searchQuery = (searchParams?.query as string) || ''
+  const searchQuery = (searchParams?.query as string) || "";
   return (
     <>
       <section className="home">
@@ -14,35 +14,27 @@ const Home = ({ searchParams} : SearchParamProps) => {
           Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
-          {
-            navLinks.slice(1, 5).map((link) => (
-              <Link
-                key={link.route}
-                href={link.route}
-                className="flex-center flex-col gap-2"
-              >
-                <li className="flex-center w-fit rounded-full bg-white p-4">
-                  <Image
-                    src={link.icon}
-                    alt="image icon"
-                    width={24}
-                    height={24}
-                  />
-                </li>
-                <p className="p-14-medium text-center text-white">
-                  {link.label}
-                </p>
-              </Link>
-            ))
-          }
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center w-fit rounded-full bg-white p-4">
+                <Image
+                  src={link.icon}
+                  alt="image icon"
+                  width={24}
+                  height={24}
+                />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
         </ul>
       </section>
       <section className="sm:mt-12">
-          <Collection
-            hasSearch
-            images={[]}
-            page={page}
-          />
+        <Collection hasSearch images={[]} page={page} />
       </section>
     </>
   );
